9c26efdd6968d656f3e543bebe3959b8bffda529
Laksafoss <anna@kerubia.dk>
2019-03-14 16:27:15 +0100

    Add Data

---
+++ R/sim_data.R
@@ -0,0 +1,48 @@
+#' Low dimensional dense simulation data
+#' 
+#' A dataset containing the result of a large scale simulation of the low 
+#' dimensional and dense setting.
+#' 
+#' @format a data frame with 8 variables:
+#' \describe{
+#'   \item{method}{the topological ordering method}
+#'   \item{n}{number of observations}
+#'   \item{p}{number of parameters}
+#'   \item{sigma}{stnadard deviation of the common error}
+#'   \item{Kendall}{Kendalls tau btween the true and esimated topological ordering}
+#'   \item{Recall}{percent of arrows true positive arrows in estimated graph}
+#'   \item{Flipped}{percent of arrows flipped compared to true graph}
+#'   \item{FDR}{percent of arrows false positivesin the estimateed graph}
+#' } 
+#' 
+"LowDense"
+
+#set.seed(299972)
+#scen <- CausDiscEqVar:::standard_senarios("LowDense")
+#LowDense <- CausDiscEqVar::sim_graph_est(scen, 50)
+#usethis::use_data(LowDense, LowDense, overwrite = TRUE)
+
+
+#' Low dimensional sparse simulation data
+#' 
+#' A dataset containing the result of a large scale simulation of the low 
+#' dimensional and sparse setting.
+#' 
+#' @format a data frame with 8 variables:
+#' \describe{
+#'   \item{method}{the topological ordering method}
+#'   \item{n}{number of observations}
+#'   \item{p}{number of parameters}
+#'   \item{sigma}{stnadard deviation of the common error}
+#'   \item{Kendall}{Kendalls tau btween the true and esimated topological ordering}
+#'   \item{Recall}{percent of arrows true positive arrows in estimated graph}
+#'   \item{Flipped}{percent of arrows flipped compared to true graph}
+#'   \item{FDR}{percent of arrows false positivesin the estimateed graph}
+#' } 
+#' 
+"LowSparse"
+
+#set.seed(700386)
+#scen <- CausDiscEqVar:::standard_senarios("LowSparse")
+#LowSparse <- CausDiscEqVar::sim_graph_est(scen, 50)
+#usethis::use_data(LowSparse, LowSparse)
